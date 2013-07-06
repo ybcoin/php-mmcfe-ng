e6b3b4226f0614450a5f8b306a8d60de25c79b4a
Sebastian Grewe <sebastian@grewe.ca>
2013-07-06 18:36:11 +0200

    Adding cache detection to many pages
    This will allow pages to skip loading data from backends like the
    database or the wallet RPC server. If a cached page is detected and
    valid, all dynamic content generation will be skipped completely.
    
    Other pages that have not been adjusted in this commit will still fetch
    backend data all the time. This will ensure clients always see the most
    recent data, like worker information or account changes.
    
    This should fix #309 completely but needs some testing.

--- public/include/pages/statistics/blocks.inc.php
+++ public/include/pages/statistics/blocks.inc.php
@@ -4,13 +4,18 @@
 if (!defined('SECURITY')) die('Hacking attempt');
 if (!$user->isAuthenticated()) header("Location: index.php?page=home");
 
-// Grab the last blocks found
-empty($_REQUEST['limit']) ? $iLimit = 20 : $iLimit = $_REQUEST['limit'];
-$aBlocksFoundData = $statistics->getBlocksFound($iLimit);
+if (!$smarty->isCached('master.tpl', md5(serialize($_REQUEST)))) {
+  $debug->append('No cached version available, fetching from backend', 3);
+  // Grab the last blocks found
+  $aBlocksFoundData = $statistics->getBlocksFound(20);
 
-// Propagate content our template
-$smarty->assign("BLOCKSFOUND", $aBlocksFoundData);
-$smarty->assign("BLOCKLIMIT", $iLimit);
+  // Propagate content our template
+  $smarty->assign("BLOCKSFOUND", $aBlocksFoundData);
+  $smarty->assign("BLOCKLIMIT", $iLimit);
+
+} else {
+  $debug->append('Using cached page', 3);
+}
 
 $smarty->assign("CONTENT", "default.tpl");
 ?>
